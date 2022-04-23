c502eb01e90f6c54496d8428b3fb0b7c3aa2439c
lcmongwe <lcmongwe@gmail.com>
2022-04-23 16:15:08 +0300

    save function created

--- credential_test.py
+++ credential_test.py
@@ -9,3 +9,12 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials=Credentials("twitter","lcmongwe","xyz",)
     def tearDown(self):
         Credentials.credentials_list=[]
+
+    def test_init(self):
+        self.assertEqual(self.new_credentials.app_name,"twitter") 
+        self.assertEqual(self.new_credentials.user_name,"lcmongwe") 
+        self.assertEqual(self.new_credentials.password,"xyz") 
+          
+    def test_save_credential(self):
+        self.new_credentials.save_contact()
+        self.assertEqual(len(Contact.contact_list),1)
