7fd956a524839a17fd97cec82fad03447bfccf2e
lcmongwe <lcmongwe@gmail.com>
2022-04-23 17:00:22 +0300

    test for adding new credentials added

--- credential_test.py
+++ credential_test.py
@@ -5,16 +5,32 @@ from credential import Credentials
 
 class TestCredentials(unittest.TestCase):
     def setUp(self):
-
         self.new_credentials=Credentials("twitter","lcmongwe","xyz",)
+
     def tearDown(self):
         Credentials.credentials_list=[]
+
+        """tests if an object instance has been created correctly"""
 
     def test_init(self):
         self.assertEqual(self.new_credentials.app_name,"twitter") 
         self.assertEqual(self.new_credentials.user_name,"lcmongwe") 
         self.assertEqual(self.new_credentials.password,"xyz") 
-          
+
+        """tests if a credential has been saved to the list"""     
     def test_save_credential(self):
-        self.new_credentials.save_contact()
-        self.assertEqual(len(Contact.contact_list),1)
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+        """tests if a new credential has been created and saved correctly"""
+    def test_save_new_credentials(self):
+        self.new_credentials.save_credentials()
+        test_credentilas = Credentials("tiktok","lucy","123") 
+        test_credentilas.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)   
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
